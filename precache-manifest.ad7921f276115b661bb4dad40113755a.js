be3e10504f2922c1b872621c2183fa3068e5fa91
jcapps@protonmail.com <jcapps@protonmail.com>
2019-08-09 22:09:16 -0500

    Updates

--- precache-manifest.ad7921f276115b661bb4dad40113755a.js
+++ precache-manifest.ad7921f276115b661bb4dad40113755a.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4d2c97bf67222d4f87ee3526f15cff45",
+    "revision": "ea24b8607cc6284526478ede39da8491",
     "url": "/bitcoin_price_converter/index.html"
   },
   {
-    "revision": "fd377a6be1a10fb7900b",
-    "url": "/bitcoin_price_converter/static/css/main.f65a89df.chunk.css"
+    "revision": "a9ebdad235e30b70e790",
+    "url": "/bitcoin_price_converter/static/css/main.fbfe6871.chunk.css"
   },
   {
     "revision": "5c7badd242f9ece752a2",
     "url": "/bitcoin_price_converter/static/js/2.adfc45b8.chunk.js"
   },
   {
-    "revision": "fd377a6be1a10fb7900b",
+    "revision": "a9ebdad235e30b70e790",
     "url": "/bitcoin_price_converter/static/js/main.a8830ad8.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bitcoin_price_converter/precache-manifest.20fc49dfffab394317391fb9cbdf82a7.js"
+  "/bitcoin_price_converter/precache-manifest.ad7921f276115b661bb4dad40113755a.js"
 );
 
 self.addEventListener('message', (event) => {
