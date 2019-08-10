8623aa5e42c9184ed2d097b539b20eee25015472
jcapps@protonmail.com <jcapps@protonmail.com>
2019-08-09 21:51:06 -0500

    Updates

---
+++ precache-manifest.20fc49dfffab394317391fb9cbdf82a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2c97bf67222d4f87ee3526f15cff45",
+    "url": "/bitcoin_price_converter/index.html"
+  },
+  {
+    "revision": "fd377a6be1a10fb7900b",
+    "url": "/bitcoin_price_converter/static/css/main.f65a89df.chunk.css"
+  },
+  {
+    "revision": "5c7badd242f9ece752a2",
+    "url": "/bitcoin_price_converter/static/js/2.adfc45b8.chunk.js"
+  },
+  {
+    "revision": "fd377a6be1a10fb7900b",
+    "url": "/bitcoin_price_converter/static/js/main.a8830ad8.chunk.js"
+  },
+  {
+    "revision": "e30db33b9127517788da",
+    "url": "/bitcoin_price_converter/static/js/runtime~main.a6ee1fcb.js"
+  },
+  {
+    "revision": "ef23608674e9c8a63b706b11653e7704",
+    "url": "/bitcoin_price_converter/static/media/btcbackground.ef236086.jpg"
+  }
+]);
